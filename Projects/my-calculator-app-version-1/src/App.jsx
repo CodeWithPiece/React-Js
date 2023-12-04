98be56a034bb773653efb8fab565312b627f23f7
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-04 12:12:57 +0530

    Managing Components

--- Projects/my-calculator-app-version-1/src/App.jsx
+++ Projects/my-calculator-app-version-1/src/App.jsx
@@ -1,75 +1,11 @@
 import style from "./App.module.css";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <>
       <div className={style.container}>
-        <div className={style.calculator}>
-          <h1>250</h1>
-          <input type="text" className={style.display} />
-          <div className={style.buttonContainer}>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              AC
-            </button>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              del
-            </button>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              %
-            </button>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              /
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              7
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              8
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              9
-            </button>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              x
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              4
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              5
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              6
-            </button>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              -
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              1
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              2
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              3
-            </button>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              +
-            </button>
-            <button
-              type="button"
-              className={`${style.button} ${style.button0} btn btn-primary`}
-            >
-              0
-            </button>
-            <button type="button" className={`${style.button} btn btn-primary`}>
-              .
-            </button>
-            <button type="button" className={`${style.button} btn btn-warning`}>
-              =
-            </button>
-          </div>
-        </div>
+        <Calculator/>
       </div>
     </>
   );
---
+++ Projects/my-calculator-app-version-1/src/components/InputDisplay.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./InputDisplay.module.css";
+
+const InputDisplay = () => {
+  return (
+    <>
+      <h1>250</h1>
+      <input type="text" className={style.display} />
+    </>
+  );
+};
+
+export default InputDisplay;
