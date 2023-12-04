c8af75530640566a3b3d2ae6f50022facfc51db6
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-04 13:19:02 +0530

    Handling Click

--- my-fragment/src/components/Item.jsx
+++ my-fragment/src/components/Item.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import style from "./Item.module.css";
 
 const Item = (props) => {
   const { foodItem, author } = props;
+  function clickMe(food) {
+    console.log(`${food} is being bought`);
+  }
   return (
     <>
       <li className="list-group-item list-group-item-action">
         {foodItem.food + "-" + foodItem.key + "-" + author.person}
+        <button
+          type="button"
+          className={`${style.button} btn btn-success`}
+          onClick={() => {
+            clickMe(foodItem.food);
+          }}
+        >
+          Buy
+        </button>
       </li>
     </>
   );
