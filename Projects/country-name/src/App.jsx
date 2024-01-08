f9f5b69c3b5c273c3cccde076618d7b1962557a9
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-08 16:22:38 +0530

    Country Name api added

--- Projects/country-name/src/App.jsx
+++ Projects/country-name/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const respose = await fetch(
+          "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/country.json"
+        );
+        const data = await respose.json();
+        const value = Object.values(data);
+        console.log(value);
+        setCountries(value);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10">
+            <h1>Country Table</h1>
+            <table className="table table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Country Name</th>
+                  <th scope="col">Country ISO3</th>
+                  <th scope="col">Country ISO Numeric</th>
+                  <th scope="col">Currency Name</th>
+                  <th scope="col">Currency Code</th>
+                  <th scope="col">Currency Number</th>
+                </tr>
+              </thead>
+              <tbody>
+                {countries.map((country, id) => {
+                  return (
+                    <tr key={id}>
+                      <td>{country.country_name}</td>
+                      <td>{country.country_iso3}</td>
+                      <td>{country.country_iso_numeric}</td>
+                      <td>{country.currency_name}</td>
+                      <td>{country.currency_code}</td>
+                      <td>{country.currency_number}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
