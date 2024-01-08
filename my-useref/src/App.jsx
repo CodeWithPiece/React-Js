1791bbdf05b1905c850467302523140983bb0d55
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-08 11:28:24 +0530

    useRef advantage over useState

--- my-useref/src/App.jsx
+++ my-useref/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let count = useRef(0);
+  let nameElement = useRef();
+  let emailElement = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    nameElement.current.style.background = "#ff54ee";
+    console.log(nameElement.current);
+    console.log(nameElement.current.value);
+    emailElement.current.style.background = "#ff54ee";
+    console.log(emailElement.current);
+    console.log(emailElement.current.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input ref={nameElement} type="text" placeholder="Enter name" />
+        <br />
+        <br />
+        <input ref={emailElement} type="text" placeholder="Enter email" />
+        <br />
+        <br />
+        <button type="submit">Submit Form</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
