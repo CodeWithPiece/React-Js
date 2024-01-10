b21d27b97c9c60dfd7d7e89bc4a313b01edb5a84
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-10 15:00:50 +0530

    Update TodoContext.jsx

--- my-useReducer-project/src/store/TodoContext.jsx
+++ my-useReducer-project/src/store/TodoContext.jsx
@@ -1,25 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 export const TodoContext = createContext();
 
+const initialState = [
+  { item: "Food" },
+  { item: "Snacks" },
+  { item: "Delicious" },
+];
+
+const reducer = (state, action) => {
+  let newState = state;
+  if (action.type === "ADD_ITEM") {
+    return [...newState, { item: action.payload.newTodo }];
+  }
+  if (action.type === "DELETE_ITEM") {
+    return newState.filter((todo) => {
+      return todo.item !== action.payload.deleteTodoItem;
+    });
+  }
+  return newState;
+};
+
 const TodoContextProvider = (props) => {
-  const [todos, setTodos] = useState([
-    { item: "Food" },
-    { item: "Snacks" },
-    { item: "Delicious" },
-  ]);
+  const [todos, dispatch] = useReducer(reducer, initialState);
 
   const addTodo = (newTodo) => {
-    setTodos((currentTodo) => {
-      return [...currentTodo, { item: newTodo }];
+    dispatch({
+      type: "ADD_ITEM",
+      payload: {
+        newTodo,
+      },
     });
   };
 
   const deleteTodo = (deleteTodoItem) => {
-    setTodos((currentTodo) => {
-      return currentTodo.filter((todo) => {
-        return todo.item !== deleteTodoItem;
-      });
+    dispatch({
+      type: "DELETE_ITEM",
+      payload: {
+        deleteTodoItem,
+      },
     });
   };
   return (
