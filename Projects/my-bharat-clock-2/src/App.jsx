0c103d16e7a57cbf8b0dcf9cc1a9f7646b29354f
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-13 09:31:17 +0530

    Working on clock project version2

---
+++ Projects/my-bharat-clock-2/src/App.jsx
@@ -0,0 +1,13 @@
+import Clock from "./components/Clock";
+import ClockTitle from "./components/ClockTitle";
+
+function App() {
+  return (
+    <div className="container">
+      <ClockTitle />
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
