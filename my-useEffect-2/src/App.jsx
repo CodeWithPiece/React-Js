6f440fada9e96865bfd64838ee494bb3f1ffe38c
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-16 11:45:41 +0530

    Implementing useEffect

--- my-useEffect-2/src/App.jsx
+++ my-useEffect-2/src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [count, setCount] = useState(0);
+  const [state, setState] = useState(true);
+  useEffect(() => {
+    console.log("Mount useEffect...!!");
+  }, []);
+
+  useEffect(() => {
+    console.log("Update useEffect...!!");
+  }, [count]);
+
   return (
     <>
-      <div>Use Effect</div>
+      {state && <h1>Count: {count}</h1>}
+      <button
+        onClick={() => {
+          setCount(count++);
+        }}
+      >
+        Click
+      </button>
+      <button>Toggle</button>
     </>
   );
 }
