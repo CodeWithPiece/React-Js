24192c072bde221e95954a61f01b9189b70272d4
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-20 10:24:41 +0530

    RouterProvider, createBrowserRouter, Outlet, Link

--- my-react-router-2/src/main.jsx
+++ my-react-router-2/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Profile from "./components/Profile.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/about", element: <About /> },
+      { path: "/home", element: <Home /> },
+      { path: "/profile", element: <Profile /> },
+    ],
+  },
+  { path: "/", element: <App /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
