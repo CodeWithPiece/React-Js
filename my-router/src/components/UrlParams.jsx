b3768895fa82c40769e9e3d4a6f53173c6376eb1
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-20 17:48:14 +0530

    Understanding useSearchParams

--- my-router/src/components/UrlParams.jsx
+++ my-router/src/components/UrlParams.jsx
@@ -5,9 +5,25 @@ const UrlParams = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   console.log(searchParams.get("name"));
   console.log(searchParams.get("city"));
+  console.log(searchParams.get("number"));
+  console.log(searchParams.get("job"));
 
   const handleClick = (event) => {
-    setSearchParams({ name: "Nirmal Kumar Pahadi", city: "Ranchi" });
+    setSearchParams({
+      name: "Nirmal Kumar Pahadi",
+      city: "Ranchi",
+      number: 9898989898,
+      job: "Software Developer",
+    });
+  };
+
+  const handleChange = (event) => {
+    setSearchParams({
+      name: "Nirmal Kumar Pahadi",
+      city: "Ranchi",
+      number: 9898989898,
+      job: event.target.value,
+    });
   };
 
   return (
@@ -22,6 +38,12 @@ const UrlParams = () => {
       >
         Update Url
       </button>
+      <input
+        type="text"
+        name="job"
+        placeholder="Search Job"
+        onChange={handleChange}
+      />
     </>
   );
 };
