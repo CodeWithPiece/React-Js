d735475a86f08ab65c6dbb1f5ebebe5e6e4b23f1
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-21 16:03:20 +0530

    Finishing Nested Routing

--- my-react-router-final/src/App.jsx
+++ my-react-router-final/src/App.jsx
@@ -6,6 +6,8 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import ErrorPage from "./components/ErrorPage";
 import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
+import Setting from "./components/Setting";
 
 const App = () => {
   return (
@@ -15,7 +17,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/" element={<Dashboard />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="setting" element={<Setting />} />
+        </Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
--- my-react-router-final/src/components/Dashboard.jsx
+++ my-react-router-final/src/components/Dashboard.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+      <nav>
+        <div className="navbar">
+          <ul className="nav-link">
+            <li>
+              <NavLink
+                to="profile"
+                className={({ isActive }) => {
+                  return isActive ? "active" : "";
+                }}
+              >
+                Profile
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="setting"
+                className={({ isActive }) => {
+                  return isActive ? "active" : "";
+                }}
+              >
+                Setting
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Outlet/>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
