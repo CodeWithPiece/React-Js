2a6dd289f1356b8a90f2960faa8dc9bff57285ee
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-21 15:38:15 +0530

    404 page routing

--- my-react-router-final/src/App.jsx
+++ my-react-router-final/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import ErrorPage from "./components/ErrorPage";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
