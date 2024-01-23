e0cf19aadd5e1ee35adb53e644d7d36dbeb1591c
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-23 10:25:07 +0530

    Revising Redux

---
+++ my-redux-3/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="d-inline-flex">
+        <button type="button" class="btn btn-success me-2">
+          Success
+        </button>
+        <input type="text" class="form-control w-25"></input>
+        <button type="button" class="btn btn-danger ms-2">
+          Danger
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
