f424cf6085ef7e8255977574a472596014e469ff
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-25 10:43:43 +0530

    Learning New Projects In React

---
+++ Projects/docs-mini-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Background from "./components/Background";
+
+const App = () => {
+  return (
+    <div className="relative w-full h-screen bg-zinc-800">
+      <Background />
+      <div className="fixed z-[3] top-0 left-0 w-full h-full"></div>
+    </div>
+  );
+};
+
+export default App;
