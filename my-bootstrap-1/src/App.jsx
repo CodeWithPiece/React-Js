4db46a3a59f9feec08a090127565e38c197fa075
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-01-26 20:06:59 +0530

    Understanding MUI basic

--- my-bootstrap-1/src/App.jsx
+++ my-bootstrap-1/src/App.jsx
@@ -1,7 +1,67 @@
 import "./App.css";
+import { Typography, Autocomplete, TextField, Box } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
 
 const App = () => {
-  return <><h1>MUI</h1></>;
+  const top100Films = [
+    { label: "The Godfather", id: 1 },
+    { label: "Pulp Fiction", id: 2 },
+  ];
+
+  return (
+    <>
+      <Typography variant="h2" component="h2" sx={{ color: "red" }}>
+        h1. Heading
+      </Typography>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={top100Films}
+        sx={{ width: 300 }}
+        renderInput={(params) => {
+          return <TextField {...params} label="Movie" />;
+        }}
+      />
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Enter Name" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+      </Box>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <IconButton sx={{ p: "10px" }} aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Google Maps"
+          inputProps={{ "aria-label": "search google maps" }}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
+          <DirectionsIcon />
+        </IconButton>
+      </Paper>
+    </>
+  );
 };
 
 export default App;
