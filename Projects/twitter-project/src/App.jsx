80affa6475b247fd9d10ec63fc17e22d606ca4ad
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-02-05 23:35:01 +0530

    Designing some pages

--- Projects/twitter-project/src/App.jsx
+++ Projects/twitter-project/src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello There</h1>
+      <div className="container">
+        <div className="row parent-div">
+          <div className="col-md-3">
+            <div className="d-flex flex-column justify-content-between parent-div top-header p-1">
+              <div>
+                <div className="logo-container d-flex justify-content-center align-items-center">
+                  <img
+                    src="./images/twitlogo.svg"
+                    alt="Twitter Logo"
+                    className="main-logo"
+                  />
+                </div>
+              </div>
+              <div>
+                <h3>asdada</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-9"></div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
