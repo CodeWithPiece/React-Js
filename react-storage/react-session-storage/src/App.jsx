239b6a58e8db9fceba14e71dde978647fc9e59f0
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-02-07 11:29:58 +0530

    Session Storage section done

--- react-storage/react-session-storage/src/App.jsx
+++ react-storage/react-session-storage/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const nameRef = useRef();
+  const emailRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
+    sessionStorage.setItem("name", nameRef.current.value);
+    sessionStorage.setItem("email", emailRef.current.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Session Storage</h1>
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event);
+        }}
+      >
+        <input type="text" placeholder="Enter name" ref={nameRef} />
+        <br />
+        <input type="email" placeholder="Enter email" ref={emailRef} />
+        <br />
+        <button>Submit Data</button>
+      </form>
+      <p>Hello, {sessionStorage.getItem("name")}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
