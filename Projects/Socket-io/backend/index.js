fb6f3a2e90517e6fe04ec29cb09853d8e5026a00
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-02-13 21:50:58 +0530

    Understanding different approach

--- Projects/Socket-io/backend/index.js
+++ Projects/Socket-io/backend/index.js
@@ -1,15 +1,46 @@
+// const express = require("express");
+// const { Server } = require("socket.io");
+// const http = require("http");
+// const cors = require("cors");
+// const PORT = 3000;
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     credentials: true,
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   console.log(`Id: ${socket.id}`);
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Listening to PORT: ${PORT}`);
+// });
+
+
+
 const express = require("express");
-const { Server } = require("socket.io");
-const http = require("http");
+const socket = require("socket.io");
 const cors = require("cors");
 const PORT = 3000;
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
+const server = app.listen(PORT, () => {
+  console.log(`Listening to PORT: ${PORT}`);
+});
+const io = socket(server, {
   cors: {
-    credentials: true,
-    origin: "*",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -20,8 +51,4 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("a user connected");
   console.log(`Id: ${socket.id}`);
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening to PORT: ${PORT}`);
 });
--- Projects/Socket-io/frontend/src/App.jsx
+++ Projects/Socket-io/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { io } from "socket.io-client";
 
 const App = () => {
-  const socket = io("http://localhost:3000/");
+  const socket = io("http://localhost:3000/", { transports: ["websocket"] });
   return <div>App</div>;
 };
 
