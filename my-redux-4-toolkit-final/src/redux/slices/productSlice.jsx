46d09c73151619ac1a996b20c3d8044ee9a3d3e0
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2024-02-21 16:44:28 +0530

    Cleaning project

--- my-redux-4-toolkit-final/src/redux/slices/productSlice.jsx
+++ my-redux-4-toolkit-final/src/redux/slices/productSlice.jsx
@@ -5,12 +5,10 @@ const productSlice = createSlice({
   initialState: [],
   reducers: {
     addAll: (state, action) => {
-      let newState = state;
-      return newState.concat(action.payload.productsData);
+      return state.concat(action.payload.productsData);
     },
     remove: (state, action) => {
-      let newState = state;
-      return newState.filter((product) => {
+      return state.filter((product) => {
         return action.payload.product.id != product.id;
       });
     },
